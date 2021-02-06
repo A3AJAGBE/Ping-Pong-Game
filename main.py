5478995bbc969dc61ce87b885e771c82490b9e8b
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-06 06:35:51 +0000

    Add key press setup

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from turtle import Screen
+from turtle import Screen, Turtle
+from paddle import Paddle
 
 screen = Screen()
 screen.setup(width=1000, height=600)
 screen.title("A3AJAGBE PING PONG GAME")
-screen.bgcolor("crimson")
+screen.bgcolor("firebrick")
+
+paddle = Paddle()
+
+# Liston for key press
+screen.listen()
+screen.onkey(paddle.right_up, 'i')
+screen.onkey(paddle.right_down, 'm')
+screen.onkey(paddle.left_up, "w")
+screen.onkey(paddle.left_down, "z")
 
 # Stay running until exited
 screen.exitonclick()
