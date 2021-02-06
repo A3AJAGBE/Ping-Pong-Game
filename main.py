ab369353171f9be1ee924b59d18ef484c7b7b9f5
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-06 05:32:28 +0000

    Add the screen setup

---
+++ main.py
@@ -0,0 +1,9 @@
+from turtle import Screen
+
+screen = Screen()
+screen.setup(width=1000, height=600)
+screen.title("A3AJAGBE PING PONG GAME")
+screen.bgcolor("crimson")
+
+# Stay running until exited
+screen.exitonclick()
