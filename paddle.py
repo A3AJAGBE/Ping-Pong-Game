cd9755d2400a22c84fe39a0e32166e12214140bc
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-06 06:56:14 +0000

    Refactor code

--- paddle.py
+++ paddle.py
@@ -1,45 +1,24 @@
 from turtle import Turtle
 
 
-class Paddle:
+class Paddle(Turtle):
 
-    def __init__(self):
-        self.right = Turtle("square")
-        self.right_paddle()
-        self.left = Turtle("square")
-        self.left_paddle()
+    def __init__(self, position):
+        super().__init__()
+        self.penup()
+        self.shape("square")
+        self.shapesize(stretch_wid=7, stretch_len=1)
+        self.goto(position)
 
-    def right_paddle(self):
-        """This function sets the right paddle"""
-        self.right.penup()
-        self.right.shapesize(stretch_wid=7, stretch_len=1)
-        self.right.goto(x=470, y=0)
+    # Set the up and down direction of the paddles
+    def up(self):
+        update_PosY = self.ycor() + 20
+        posX = self.xcor()
+        self.goto(x=posX, y=update_PosY)
 
-    def left_paddle(self):
-        """This function sets the left paddle"""
-        self.left.penup()
-        self.left.shapesize(stretch_wid=7, stretch_len=1)
-        self.left.goto(x=-480, y=0)
+    def down(self):
+        update_PosY = self.ycor() - 20
+        posX = self.xcor()
+        self.goto(x=posX, y=update_PosY)
 
-    # Set the up and down direction of the right paddle
-    def right_up(self):
-        update_PosY = self.right.ycor() + 20
-        posX = self.right.xcor()
-        self.right.goto(x=posX, y=update_PosY)
-
-    def right_down(self):
-        update_PosY = self.right.ycor() - 20
-        posX = self.right.xcor()
-        self.right.goto(x=posX, y=update_PosY)
-
-    # Set the up and down direction of the left paddle
-    def left_up(self):
-        update_PosY = self.left.ycor() + 20
-        posX = self.left.xcor()
-        self.left.goto(x=posX, y=update_PosY)
-
-    def left_down(self):
-        update_PosY = self.left.ycor() - 20
-        posX = self.left.xcor()
-        self.left.goto(x=posX, y=update_PosY)
 
